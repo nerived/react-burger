649868bfca465d15fbb68880bdc29f7f33f2ab4e
Andrey Kulakovskiy <kulakovskiy.a@ifuture.by>
2024-09-05 09:53:35 +0300

    sprint-6/step-1, fixed review

--- cypress/e2e/Constructor/constructor.cy.ts
+++ cypress/e2e/Constructor/constructor.cy.ts
@@ -1,80 +1,76 @@
+import {
+  bunIngredient,
+  constructorSpace,
+  constructorBunSpace,
+  modalClose,
+  modalIngredient,
+  modalOrder,
+  ingredientName,
+  ingredientCalories,
+  ingredientProteins,
+  ingredientFat,
+  ingredientCarbohydrates,
+  placeOrderButton,
+  orderNumber,
+  orderName,
+} from "../../support/elements-selectors";
+
 describe("Constructor", () => {
   beforeEach(() => {
     window.localStorage.setItem(
       "accessToken",
-      JSON.stringify(
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2ZDhiZTk1MTE5ZDQ1MDAxYjUwNDRiYSIsImlhdCI6MTcyNTQ4MjY4NCwiZXhwIjoxNzI1NDgzODg0fQ.ks4D8dpDlHXZVVejZarNpRGJChOWmpjUvXCpblZ6Q3w"
-      )
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9"
     );
-    cy.visit("http://localhost:3000");
+    cy.visit("");
     cy.intercept("GET", "api/ingredients", { fixture: "ingredients.json" });
     cy.intercept("GET", "api/auth/user", { fixture: "user.json" });
     cy.intercept("POST", "api/orders", { fixture: "order.json" });
   });
 
   it("should contain ingredient after DND", () => {
-    cy.get("[data-testid=643d69a5c3f7b9001cfa093c]").trigger("dragstart");
-    cy.get("[data-testid=constructor-empty]").trigger("drop");
-    cy.get("[data-testid=643d69a5c3f7b9001cfa0941]").trigger("dragstart");
-    cy.get("[data-testid=constructor-empty]").trigger("drop");
-    cy.get("[data-testid=643d69a5c3f7b9001cfa0942]").trigger("dragstart");
-    cy.get("[data-testid=constructor]").trigger("drop");
+    cy.fillConstuctor();
 
-    cy.get("[data-testid=constructor-bun]").should(
-      "contain",
-      "Краторная булка N-200i"
-    );
-    cy.get("[data-testid=constructor]").should(
+    cy.get(constructorBunSpace).should("contain", "Краторная булка N-200i");
+    cy.get(constructorSpace).should(
       "contain",
       "Биокотлета из марсианской Магнолии"
     );
-    cy.get("[data-testid=constructor]").should("contain", "Соус Spicy-X");
+    cy.get(constructorSpace).should("contain", "Соус Spicy-X");
   });
 
   it("should open modal by click on ingredient and close modal after click close icon", () => {
-    cy.get("[data-testid=643d69a5c3f7b9001cfa093c]").click();
-    cy.get("[data-testid=modal-ingredient]").should("exist");
-    cy.get("[data-testid=modal-close]").click();
-    cy.get("[data-testid=modal-ingredient]").should("not.exist");
+    cy.get(bunIngredient).click();
+    cy.get(modalIngredient).should("exist");
+    cy.get(modalClose).click();
+    cy.get(modalIngredient).should("not.exist");
   });
 
   it("should open modal by click on ingredient and close modal after click ESC key", () => {
-    cy.get("[data-testid=643d69a5c3f7b9001cfa093c]").click();
-    cy.get("[data-testid=modal-ingredient]").should("exist");
+    cy.get(bunIngredient).click();
+    cy.get(modalIngredient).should("exist");
     cy.get("body").trigger("keydown", { key: "Escape" });
-    cy.get("[data-testid=modal-ingredient]").should("not.exist");
+    cy.get(modalIngredient).should("not.exist");
   });
 
   it("should open modal and contain ingredient details after click", () => {
-    cy.get("[data-testid=643d69a5c3f7b9001cfa093c]").click();
-    cy.get("[data-testid=ingredient-name]").should(
-      "have.text",
-      "Краторная булка N-200i"
-    );
-
-    cy.get("[data-testid=ingredient-calories]").should("have.text", 420);
-
-    cy.get("[data-testid=ingredient-proteins]").should("have.text", 80);
-
-    cy.get("[data-testid=ingredient-fat]").should("have.text", 24);
-
-    cy.get("[data-testid=ingredient-carbohydrates]").should("have.text", 53);
+    cy.get(bunIngredient).click();
+    cy.get(ingredientName).should("have.text", "Краторная булка N-200i");
+    cy.get(ingredientCalories).should("have.text", 420);
+    cy.get(ingredientProteins).should("have.text", 80);
+    cy.get(ingredientFat).should("have.text", 24);
+    cy.get(ingredientCarbohydrates).should("have.text", 53);
   });
 
   it("should open order modal after order button click ", () => {
-    cy.get("[data-testid=643d69a5c3f7b9001cfa093c]").trigger("dragstart");
-    cy.get("[data-testid=constructor-empty]").trigger("drop");
-    cy.get("[data-testid=643d69a5c3f7b9001cfa0941]").trigger("dragstart");
-    cy.get("[data-testid=constructor-empty]").trigger("drop");
-    cy.get("[data-testid=643d69a5c3f7b9001cfa0942]").trigger("dragstart");
-    cy.get("[data-testid=constructor]").trigger("drop");
-    cy.get("[data-testid=place-order-button]").click();
+    cy.fillConstuctor();
+
+    cy.get(placeOrderButton).click();
     // eslint-disable-next-line cypress/no-unnecessary-waiting
     cy.wait(500);
-    cy.get("[data-testid=place-order-button]").click();
-    cy.get("[data-testid=modal-order]").should("exist");
-    cy.get("[data-testid=order-number]").should("have.text", 51978);
-    cy.get("[data-testid=order-name]").should(
+    cy.get(placeOrderButton).click();
+    cy.get(modalOrder).should("exist");
+    cy.get(orderNumber).should("have.text", 51978);
+    cy.get(orderName).should(
       "have.text",
       "Флюоресцентный люминесцентный бургер"
     );
---
+++ cypress/support/elements-selectors.ts
@@ -0,0 +1,22 @@
+export const bunIngredient = "[data-testid=643d69a5c3f7b9001cfa093c]";
+export const mainIngredient = "[data-testid=643d69a5c3f7b9001cfa0941]";
+export const sauceIngredient = "[data-testid=643d69a5c3f7b9001cfa0942]";
+
+export const constructorSpace = "[data-testid=constructor]";
+export const constructorEmptySpace = "[data-testid=constructor-empty]";
+export const constructorBunSpace = "[data-testid=constructor-bun]";
+
+export const modalIngredient = "[data-testid=modal-ingredient]";
+export const modalOrder = "[data-testid=modal-order]";
+export const modalClose = "[data-testid=modal-close]";
+
+export const ingredientName = "[data-testid=ingredient-name]";
+export const ingredientCalories = "[data-testid=ingredient-calories]";
+export const ingredientProteins = "[data-testid=ingredient-proteins]";
+export const ingredientFat = "[data-testid=ingredient-fat]";
+export const ingredientCarbohydrates = "[data-testid=ingredient-carbohydrates]";
+
+export const placeOrderButton = "[data-testid=place-order-button]";
+
+export const orderNumber = "[data-testid=order-number]";
+export const orderName = "[data-testid=order-name]";
